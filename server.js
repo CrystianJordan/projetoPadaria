2a992929819ce01da0b6c87020615e1e2e1585c3
Crystian Jordan <crystian.jordan@catolicasc.org.br>
2020-06-22 16:51:58 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//carregar env
+dotenv.config({
+    path: './config/config.env'
+});
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Servidor rodando na porta ${PORT}`));
